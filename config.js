7536496b778b20eecd603c0984c1a11558414a24
Shivaani168 <73694690+Shivaani168@users.noreply.github.com>
2023-05-20 22:55:56 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD7rQn5iVF_bDYDacoF-rirhZ5JctsU7qk",
+  authDomain: "students-app-30642.firebaseapp.com",
+  projectId: "students-app-30642",
+  storageBucket: "students-app-30642.appspot.com",
+  messagingSenderId: "1029524569579",
+  appId: "1:1029524569579:web:c7af408ef3c10254882dae"
+}; 
